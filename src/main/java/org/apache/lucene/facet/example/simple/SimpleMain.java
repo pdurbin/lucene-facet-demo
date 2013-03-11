90dc8ef74386bcd12db502aedf0538675924c0cf
Philip Durbin <philipdurbin@gmail.com>
2013-03-11 13:21:01 -0400

    reformat

--- src/main/java/org/apache/lucene/facet/example/simple/SimpleMain.java
+++ src/main/java/org/apache/lucene/facet/example/simple/SimpleMain.java
@@ -14,86 +14,86 @@ import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyReader;
 
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 /**
  * Driver for the simple sample.
- * 
+ *
  * @lucene.experimental
  */
 public class SimpleMain {
 
-  /**
-   * Driver for the simple sample.
-   * @throws Exception on error (no detailed exception handling here for sample simplicity
-   */
-  public static void main(String[] args) throws Exception {
-    new SimpleMain().runSimple();
-    new SimpleMain().runDrillDown().getFacetResults();
-    ExampleUtils.log("DONE");
-  }
+    /**
+     * Driver for the simple sample.
+     *
+     * @throws Exception on error (no detailed exception handling here for
+     * sample simplicity
+     */
+    public static void main(String[] args) throws Exception {
+        new SimpleMain().runSimple();
+        new SimpleMain().runDrillDown().getFacetResults();
+        ExampleUtils.log("DONE");
+    }
 
-  public ExampleResult runSimple() throws Exception {
-    // create Directories for the search index and for the taxonomy index
-    Directory indexDir = new RAMDirectory();
-    Directory taxoDir = new RAMDirectory();
+    public ExampleResult runSimple() throws Exception {
+        // create Directories for the search index and for the taxonomy index
+        Directory indexDir = new RAMDirectory();
+        Directory taxoDir = new RAMDirectory();
 
-    // index the sample documents
-    ExampleUtils.log("index the sample documents...");
-    SimpleIndexer.index(indexDir, taxoDir);
+        // index the sample documents
+        ExampleUtils.log("index the sample documents...");
+        SimpleIndexer.index(indexDir, taxoDir);
 
-    // open readers
-    TaxonomyReader taxo = new DirectoryTaxonomyReader(taxoDir);
-    IndexReader indexReader = IndexReader.open(indexDir, true);
+        // open readers
+        TaxonomyReader taxo = new DirectoryTaxonomyReader(taxoDir);
+        IndexReader indexReader = IndexReader.open(indexDir, true);
 
-    ExampleUtils.log("search the sample documents...");
-    List<FacetResult> facetRes = SimpleSearcher.searchWithFacets(indexReader, taxo);
+        ExampleUtils.log("search the sample documents...");
+        List<FacetResult> facetRes = SimpleSearcher.searchWithFacets(indexReader, taxo);
 
-    // close readers
-    taxo.close();
-    indexReader.close();
-    
-    ExampleResult res = new ExampleResult();
-    res.setFacetResults(facetRes);
-    return res;
-  }
+        // close readers
+        taxo.close();
+        indexReader.close();
 
-  public ExampleResult runDrillDown() throws Exception {
-    // create Directories for the search index and for the taxonomy index
-    Directory indexDir = new RAMDirectory();
-    Directory taxoDir = new RAMDirectory();
-    
-    // index the sample documents
-    ExampleUtils.log("index the sample documents...");
-    SimpleIndexer.index(indexDir, taxoDir);
+        ExampleResult res = new ExampleResult();
+        res.setFacetResults(facetRes);
+        return res;
+    }
 
-    // open readers
-    TaxonomyReader taxo = new DirectoryTaxonomyReader(taxoDir);
-    IndexReader indexReader = IndexReader.open(indexDir, true);
+    public ExampleResult runDrillDown() throws Exception {
+        // create Directories for the search index and for the taxonomy index
+        Directory indexDir = new RAMDirectory();
+        Directory taxoDir = new RAMDirectory();
 
-    ExampleUtils.log("search the sample documents...");
-    List<FacetResult> facetRes = SimpleSearcher.searchWithDrillDown(indexReader, taxo);
-    
-    // close readers
-    taxo.close();
-    indexReader.close();
-    
-    ExampleResult res = new ExampleResult();
-    res.setFacetResults(facetRes);
-    return res;
-  }
+        // index the sample documents
+        ExampleUtils.log("index the sample documents...");
+        SimpleIndexer.index(indexDir, taxoDir);
 
+        // open readers
+        TaxonomyReader taxo = new DirectoryTaxonomyReader(taxoDir);
+        IndexReader indexReader = IndexReader.open(indexDir, true);
+
+        ExampleUtils.log("search the sample documents...");
+        List<FacetResult> facetRes = SimpleSearcher.searchWithDrillDown(indexReader, taxo);
+
+        // close readers
+        taxo.close();
+        indexReader.close();
+
+        ExampleResult res = new ExampleResult();
+        res.setFacetResults(facetRes);
+        return res;
+    }
 }
